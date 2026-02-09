4bde78350e07708acbdb70a559224c5c0937bf40
yasinelmoutaouakel-creator <yasinelmoutaouakel@gmail.com>
2026-02-09 22:17:44 +0000

    Fix build errors

--- src/components/shared/LoginView.jsx
+++ src/components/shared/LoginView.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { LogIn, Lock, User, Coffee } from 'lucide-react';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from '../../contexts/AuthContext.jsx';
 
 const LoginView = () => {
   const [username, setUsername] = useState('');
